75d44789a1363c72b982a563970cbd1efb62efb8
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-07-16 12:27:21 -0400

    running into CORS rejection from localhost

--- src/UserQuestionField.jsx
+++ src/UserQuestionField.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 
-const UserQuestionField = (userQuestion) => {
-    const submitAPIRequest = async (userQuestion) =>{
+const UserQuestionField = () => {
+    const handleSubmit = async (userQuestion) =>{
         const response = await fetch(`https://us-east4-glassy-signal-354012.cloudfunctions.net/get_answer/?message=${userQuestion}`)
-        alert(response.json())
+        console.log(response.json())
     }
 
     return (
         <div>
-            <form onSubmit={alert("submitted")}>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                handleSubmit(e.value)
+            }}>
                 <input type="text"></input>
-                <input type="submit"></input>
+                <input type="submit" value="submit"></input>
             </form>
         </div>
     )
